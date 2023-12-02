67d5779c695bc28193887f43f9b22d5fc257e380
Drewski2222 <dreisner2222@gmail.com>
2023-12-02 13:33:40 -0500

    Implemented CLI to determine sorting key and method

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ from merge_sort import merge_sort
 from quick_sort import quick_sort
 
 def main():
+    sort_key = ''
+
     # load COVID data
     report = covid.get_report()
 
-    # available keys to sort by: cases, deaths, rate
-    sort_key = 'Cases'
+    # available keys to sort by: Cases, Deaths, Rate
+    print("Key to sort data by? (1 = cases, 2 = deaths, 3 = rate)")
+    choice = input()
+
+    if choice == '1':
+        sort_key = 'Cases'
+    elif choice == '2':
+        sort_key = 'Deaths'
+    elif choice == '3':
+        sort_key = 'Rate'
 
     # print first item in loaded report for debugging
     '''
@@ -18,25 +28,31 @@ def main():
         print("Dataset is empty or not loaded correctly")
     '''
 
-    # start timer
-    time1 = time.time()
+    print("Sorting algorithm? (1 = merge, 2 = quick)")
+    sort_method = input()
 
-    # merge sort
-    merge_sort(report, sort_key)
+    if sort_method == '1':
+        # merge sort
+        # start timer
+        time1 = time.time()
+        merge_sort(report, sort_key)
+    elif sort_method == '2':
+        # quick sort
+        # start timer
+        time1 = time.time()
+        quick_sort(report, sort_key)
 
     time2 = time.time()
     time3 = time2 - time1
 
-    print(f"Merge sort completed in {time3} seconds")
+    if sort_method == '1':
+        print(f"Merge sort completed in {time3} seconds")
+    elif sort_method == '2':
+        print(f"Quick sort completed in {time3} seconds")
 
     # print first 10 items of sorted data
     print("First 10 items in sorted result:")
     for item in report[:10]:
-        print(item)
-
-    sorted_report = quick_sort(report, sort_key)
-
-    for item in sorted_report[:10]:
         print(item)
 
 if __name__ == "__main__":
